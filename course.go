b70db5f8be2ec5a0455962bcff4dbb02c640af67
Lezcano Emanuel Ezequiel <emanuel.pasante@gmail.com>
2023-03-08 07:28:42 -0300

    structure export

--- course.go
+++ course.go
@@ -2,32 +2,34 @@ package methods
 
 import "fmt"
 
-
-type course struct {
+type Course struct {
 	Name    string
 	Price   float64
 	IsFree  bool
 	UserIDs []uint
 	Classes map[uint]string
 }
-func NewCourse(name string, price float64, isFree bool) *course{
-	return &course{
-		Name: name,
-		Price: price,
+
+func NewCourse(name string, price float64, isFree bool) *Course {
+	return &Course{
+		Name:   name,
+		Price:  price,
 		IsFree: isFree,
 	}
 }
-func printText(){
+func PrintText() {
 	fmt.Println("Hi methods.")
 }
+
 //Method of struct Course
-func (c *Course) printClasses() {
+func (c *Course) PrintClasses() {
 	text := "Las clases son: "
 	for _, class := range c.Classes {
 		text += class + ", "
 	}
 	fmt.Println(text)
 }
+
 //Cambia la propiedad Price de un struct Course
 func (c *Course) changePrice() {
 	c.Price = 12.50
